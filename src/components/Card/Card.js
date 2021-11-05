234cabded1c5e34aca8043cd131dabe79a8ce68c
Arijit Patra <ari.clicks@gmail.com>
2021-11-05 20:51:47 +0530

    responsive UI changes
    responsive UI changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,9 +4,9 @@ export const Card = ({ image_url, artist, title, genre }) => {
   return (
     <section>
       <img loading="lazy" src={image_url} alt={`${artist}-${title}`} />
-      <h2>{artist}</h2>
-      <h5>{title}</h5>
-      <h6>{genre}</h6>
+      <h2 title={artist}>{artist}</h2>
+      <h5 title={title}>{title}</h5>
+      <h6 title={genre}>{genre}</h6>
     </section>
   );
 };
